1c8a9bff47db5a7f161a12a8933c7ed728efafb2
AdrianWojtowicz <adrian.wojtowicz@op.pl>
2022-03-01 21:23:27 +0100

    zad1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('div.gllery');
+
+galleryItems.forEach( element => {
+    const galleryItem = document.createElement('div');
+    galleryItem.classList.add('gallery__item');
+
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__link');
+    galleryLink.href = photo.original;
+
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image');
+    galleryImage.dataset.source = element.original;
+    galleryImage.alt = element.description;
+    galleryImage.src = element.preview;
+
+    galleryBox.append(galleryItem);
+    galleryItem.append(galleryLink);
+    galleryLink.append(galleryImage);
+});
+
+galleryBox.addEventListener('click', e => {
+    e.preventDefault();
+    basicLightbox.create('<img width="1280" src = ${e.target.dataset.source}>').show();
+});
